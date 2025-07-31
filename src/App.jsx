84605e5f00261bba3b4f485b7c9bd872e620bbfa
jhlarry1109 <jhlarry1109@gmail.com>
2025-07-31 16:09:38 +0900

    feat: useRef로 div 참조, useEffect로 마운트 시 테두리 스타일 적용 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  const name = "류승찬";
+  const divRef = useRef(null)
+  
+  useEffect(() => {
+    console.log('컴포넌트 마운트')
+    divRef.current.style.border = '1px solid red'
+  }, [])
 
   return (
-    <>
-      화이팅
-    </>
+    <div ref = {divRef}>
+      <h1>Hook 과제</h1>
+    </div>
   );
 }
 
