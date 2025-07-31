fca3b674c45792db769b76d73add00945bc412c3
harinsd404 <lohyen09@gmail.com>
2025-07-31 13:24:22 +0900

    feat: 타이머 시간 재는 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import TimerDisplay from './components/TimerDisplay';
 import TimerControls from './components/TimerControls';
 
 function App() {
+  const [time, setTime] = useState(1500);
+  useEffect(()=>{
+      const interval = setInterval(()=>{
+        setTime(time=>time-1);
+      }, 1000);
+      if(time===0){
+        setTime(300);
+      }
+  }, [time]);
 
   return (
     <>
-      <TimerDisplay />
-      <TimerControls />
+      <TimerDisplay time={time} />
     </>
   );
 }
--- src/components/TimerDisplay.jsx
+++ src/components/TimerDisplay.jsx
@@ -1,8 +1,16 @@
-export default function TimerDisplay(){
+import {useRef, useEffect} from "react";
+
+export default function TimerDisplay({time}){
+    const timeRef = useRef(null);
+    useEffect(()=>{
+        const timeText = `${String(Math.floor(time / 60)).padStart(2, '0')} : ${String(Math.floor(time%60)).padStart(2, '0')}`;
+        if(timeRef.current)
+            timeRef.current.textContent = timeText;
+    }, [time]);
     return (
         <>
             <h1>🍅뽀모도로 타이머🍅</h1>
-            <h2>00 : 00</h2>
+            <h2 ref={timeRef}>00 : 00</h2>
         </>
     );
 }
