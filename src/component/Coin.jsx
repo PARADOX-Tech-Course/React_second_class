492011c9ace018510ba428f46b6c5d2252353b11
Kr-Verified <dmschd0573@gmail.com>
2025-07-31 15:37:59 +0900

    refactor :: coin 수정

--- src/component/Coin.jsx
+++ src/component/Coin.jsx
@@ -1,25 +1,20 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 
 export default function Coin() {
   const [coin, setCoin] = useState(0);
   const div = useRef(null);
-  const text = useRef(null);
   
   const earn = () => {
     if (div.current) div.current.style.backgroundColor = `rgba(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255}, ${Math.random()%0.5})`;
     setCoin(coin+1);
   };
 
-  useEffect(() => {
-    if (text.current) text.current.textContent = coin;
-  }, [coin]);
-
   return (
     <div ref={div} style={{
         position: "absolute",
         inset: 0,
       }}>
-      <p ref={text} style={{fontWeight: 'bolder'}}>0</p>
+      <p style={{fontWeight: 'bolder'}}>{coin}$</p>
       <button onClick={earn}>Earn</button>
     </div>
   )
