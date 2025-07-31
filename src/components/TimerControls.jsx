5a74eaf8851a1417752a36542cb2b86e83658800
harinsd404 <lohyen09@gmail.com>
2025-07-31 12:04:14 +0900

    feat: 타이머 컴포넌트 기본 UI 추가

---
+++ src/components/TimerControls.jsx
@@ -0,0 +1,8 @@
+export default function TimerControls(){
+    return(
+        <>
+            <button>시작하기</button>
+            <button>공부 📓</button>
+        </>
+    )
+}
---
+++ src/components/TimerDisplay.jsx
@@ -0,0 +1,8 @@
+export default function TimerDisplay(){
+    return (
+        <>
+            <h1>🍅뽀모도로 타이머🍅</h1>
+            <h2>00 : 00</h2>
+        </>
+    );
+}
